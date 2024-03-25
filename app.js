349f5edc9ee4df832d9e0687ed971f8e2705fb60
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-26 01:46:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var animals = ["Lion", "Cat", "Dog"];
+for (var _i = 0, animals_1 = animals; _i < animals_1.length; _i++) {
+    var animal = animals_1[_i];
+    console.log(animal);
+}
+console.log("\n");
+for (var _a = 0, animals_2 = animals; _a < animals_2.length; _a++) {
+    var animal = animals_2[_a];
+    console.log("A ".concat(animal, " Has a tail"));
+}
+console.log("\n All of there are great pets but Dog is a loyalty one");
---
+++ app.ts
@@ -0,0 +1,11 @@
+// let animals : string [] = ["Lion" , "Cat" , "Dog"]
+// for (let  animal of animals) {
+//     console.log (animal)
+// }
+// console.log ("\n")
+
+// for (let animal of animals) {
+//     console.log (`A ${animal} Has a tail`)
+// }
+
+// console.log ("\n All of there are great pets but Dog is a loyalty one")
